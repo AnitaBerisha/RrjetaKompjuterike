5cc16cb35fcf96517e61e1b74a17bd8222f61f27
AnitaBerisha <anitaberisha999@gmail.com>
2026-04-15 14:17:09 +0200

    update

--- server.java
+++ server.java
@@ -7,3 +7,11 @@ public class server {
     // Variablat për Portin dhe IP
     private static final String IP_ADDRESS = "172.20.10.7";
     private static final int PORT = 9997;
+
+    public static void main(String[] args) {
+        // Kapaciteti për të dëgjuar të paktën 4 anëtarë (Thread Pool)
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(IP_ADDRESS))) {
+            System.out.println("Serveri u nis në IP: " + IP_ADDRESS + " dhe Port: " + PORT);
+            System.out.println("Duke pritur kërkesat nga anëtarët e grupit...");
